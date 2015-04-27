c08e127faef560320b713a109a8ef9ac46203724
skandinaff <sevalks@gmail.com>
2015-04-27 17:09:23 +0300

    Got functions DimFew, SetFew working

--- BCM_dihalt_solution/BCM_dihalt_solution.c
+++ BCM_dihalt_solution/BCM_dihalt_solution.c
@@ -24,7 +24,6 @@ typedef unsigned char byte;
 typedef unsigned char boolean;
 
 
-
 // Brightness values are stored here:
 //unsigned char BRG[8]= {1,33,65,97,129,161,193,225};
 unsigned char BRG[8] = {15,255,255,0,0,255,255,0};
@@ -50,121 +49,106 @@ ISR(TIMER2_COMP_vect)
 {
 	OCR2 = ROR(OCR2); // That thing, where we  moving 1bit in OCR with each clock cycle
 
-	COPYBIT(PORTD,0,BRG0[0]);
-	COPYBIT(PORTD,1,BRG0[1]);
-	COPYBIT(PORTD,2,BRG0[2]);
-	COPYBIT(PORTD,3,BRG0[3]);
-	COPYBIT(PORTD,4,BRG0[4]);
-	COPYBIT(PORTD,5,BRG0[5]);
-	COPYBIT(PORTD,6,BRG0[6]);
-	COPYBIT(PORTD,7,BRG0[7]);
+	//COPYBIT(PORTD,0,BRG0[0]);
+	//COPYBIT(PORTD,1,BRG0[1]);
+	//COPYBIT(PORTD,2,BRG0[2]);
+	//COPYBIT(PORTD,3,BRG0[3]);
+	//COPYBIT(PORTD,4,BRG0[4]);
+	//COPYBIT(PORTD,5,BRG0[5]);
+	//COPYBIT(PORTD,6,BRG0[6]);
+	//COPYBIT(PORTD,7,BRG0[7]);
 
-	//for(int i = 0; i<8; i++)
-	//{
-		//COPYBIT(PORTD,i,BRG0[i]);
-	//}
-
-
-
-}
-
-
-void SetOne(unsigned char LED, unsigned char VAL)
-{
-	//if(OCR2 == 64)
-	//{
-		BRG0[LED]=VAL;
-	//}
-}
-
-void SetFew(byte LEDS, byte VAL)
-{
-	if (OCR2 == 1)
+	for(int i = 0; i<8; i++)
 	{
-		for (byte i = 0; i<8; i++)
-		{
-			if (LEDS)
-			{
-			}
-		}
+		COPYBIT(PORTD,i,BRG0[i]);
+	}
+
+
+
+}
+
+
+void SetOne(byte LED, byte VAL) { BRG0[LED]=VAL; }
+	
+void SetFew(byte LEDS, byte VAL) 
+{
+	for (byte i = 0; i < 8; i++)
+	{
+		if((LEDS >> i)&1)  BRG0[i]=VAL;
 	}
 }
 
-void DimOne(byte LED,int VAL, boolean  dir)
+void SetAll(byte VAL)
 {
-	
-	
-	TCCR1A |= 0;
-	TCCR1B |= (0<<CS12)|(1<<CS11)|(1<<CS10);
-	TCCR1B |= (1<<WGM12);
+	for (byte i = 0; i<8; i++)
+	{
+		BRG0[i]=VAL;
+	}
+}
 
-	//TIMSK |= (1<<TOIE1);
-	
-	OCR1A = VAL;
-		 
+
+void DimFew(byte LEDS, int delay, byte topVal, boolean  dir)
+{
+	TCCR1B |= (1<<WGM12)|(0<<CS12)|(1<<CS11)|(1<<CS10);
+	OCR1A = delay;
+		
+	if (dir == 1)
+	{
+		TCNT1 = 0;
+		for(int i=0; i<topVal; i++) // Rolling Brightness!
+		{
+			while(!(TIFR & (1 << OCF1A))) SetFew(LEDS,i);
+			TIFR |= (1 << OCF1A);
+		}
+	}
+	else
+	{
+		SetFew(LEDS,topVal);
+
+		TCNT1 = 0;
+		for (int i = topVal; i>0; i--)
+		{
+			while(!(TIFR & (1 << OCF1A)))  SetFew(LEDS,i);
+			TIFR |= (1 << OCF1A);
+		}
+		SetFew(LEDS,0);
+	}
+}
+
+void DimOne(byte LED, int delay, byte topVal, boolean  dir) // Delay works best in range of 10 - 600
+{
+	TCCR1B |= (1<<WGM12)|(0<<CS12)|(1<<CS11)|(1<<CS10);
+	OCR1A = delay;
+	 
 		if (dir == 1)
 		{
-
 			TCNT1 = 0;
-
-			for(int i=0; i<255; i++) // Rolling Brightness!
-			{
-				//while(TCNT1 < VAL)
-				//{
-					//if (i!=127 || i!=64)
-					//{
-						
+			for(int i=0; i<topVal; i++) // Rolling Brightness!
+			{				
 				while(!(TIFR & (1 << OCF1A))) if(OCR2==64)  SetOne(LED,i);		
-
 				TIFR |= (1 << OCF1A);
-					//}
-				//}
-				//TCNT1 = 0;
-				
-				
-			}
-			
+			}		
 		}
-		
 		else
 		{
-			TCNT1H = 0;
-			TCNT1L = 0;
-			SetOne(LED,VAL);
-			for (int i = VAL; i>0; i--)
+			SetOne(LED,topVal);
+
+			TCNT1 = 0;
+			for (int i = topVal; i>0; i--)
 			{
-				while(TCNT1L < VAL)
-				{
-					//if (i!=127 || i!=64)
-					//{
-						if(OCR2==64) SetOne(LED,i);
-					//}
-					//_delay_us(7);
-				}
-				//SetOne(LED,0);
-				TCNT1L = 0;
+				while(!(TIFR & (1 << OCF1A))) if(OCR2==64) SetOne(LED,i);
+				TIFR |= (1 << OCF1A);
 			}
 			SetOne(LED,0);
 		}
-
 }
 
 
-void SetAll(unsigned char VAL)
-{
-	//if(OCR2 == 64)
-	//{
-		for (unsigned char i = 0; i<8; i++)
-		{
-			BRG0[i]=VAL;
-		}
-	//}
-}
 
 
 void main(void)
 {
-	volatile unsigned char a;
+	volatile unsigned char a = 1;
 
 	sei(); // Allow interrupts. Yeah, because why not, interrupt me, interrupt me everybody!!!1
 
@@ -176,101 +160,32 @@ void main(void)
 
 	PORTD = 0x00; // initalizig our port
 	DDRD = 0xFF; // Setting directions as outputs
+	
 
-		
-    //DimOne(7,255,1,1);
+
 
 	while(1)
 	{
+	
+
 		
-		//SetAll(10);
-		
-		//SetOne(3,127);
-		
-		//SetOne(4,128);
+		a=ROL(a);
 		
 
-		//for (int i = 0; i<255; i++)
-		//{
-			//SetOne(0,i);
-			//_delay_ms(5);
-		//}
+		DimFew(a,3000,250,1);
+		_delay_ms(500);
+		DimFew(a,3000,250,0);
+		_delay_ms(500);
 
-		//SetAll(0);
-		
-		//SetOne(7,90);
-		//_delay_ms(1000);
-		
-		//_delay_ms(1000);
-		
-		//DimOne(6,255,1);
-		//for (int i = 0; i<8;i++)
-		//{
-			//DimOne(i,255,100,1);
-			////SetOne((i-1),0);
-			//SetAll(0);
-		//}
-		
-		//SetAll(1);
-		DimOne(7,130,1);
-		//SetOne(7,255);
-		_delay_ms(1000);
-		DimOne(7,255,0);
-		
-		SetOne(7,0);
-		_delay_ms(1000);
-		SetOne(7,0);
-		
-		
-		//_delay_ms(10);
-
-		//SetOne(7,255);
-		
-		//for (int i = 0; i<255; i++)
-		//{
-			//for (int j = 0; j<1; j++)
-			//{
-				//BRG0[j]=i;
-				//_delay_ms(10);
-			//}
-			//for (int j = 1; j<8; j++)
-			//{
-				//BRG0[j]=0;
-//
-			//}
-//
-		//}
+		//DimFew(a,600,50,1);
 		//
-		//for (int i = 255; i>0; i--)
-		//{
-			//for (int j = 0; j<1; j++)
-			//{
-				//BRG0[j]=i;
-				////_delay_ms(10);
-			//}
-			//for (int j = 1; j<8; j++)
-			//{
-				//BRG0[j]=0;
+		//_delay_ms(25);
 //
-			//}
+		//DimFew(a,600,50,0);
 //
-		//}
+		//_delay_ms(25);
+
 		
-		//for (int i = 0; i<8; i++)
-		//{
-			//for (int j = 0; j<8; j++)
-			//{
-				//BRG2[j]=BRIGHNESS_ARR[j][i];
-				//_delay_ms(200);
-			//}
-		//}
-		
-		//for (int j = 0; j<8; j++)
-			//{
-				//BRG2[j]=j+32;
-			//}
-			
-			
 	}
 
 }
